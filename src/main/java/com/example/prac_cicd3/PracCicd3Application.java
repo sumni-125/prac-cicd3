31ac8b60b7e9a6ec8742e4bdc479a66d75717ccb
sumni-125 <dltnalssumin@gmail.com>
2025-06-11 09:20:50 +0900

    cicd

---
+++ src/main/java/com/example/prac_cicd3/PracCicd3Application.java
@@ -0,0 +1,13 @@
+package com.example.prac_cicd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracCicd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracCicd3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prac_cicd3/PracCicd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac_cicd3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracCicd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
